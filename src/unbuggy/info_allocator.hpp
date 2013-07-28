a790fd31d02a5ac2f8218452c2799ca4a170bb04
Jeff Schwab <jeff@schwabcenter.com>
2013-07-27 20:47:47 -0400

    Let info_allocator wrap an underlying allocator.

--- src/unbuggy/info_allocator.hpp
+++ src/unbuggy/info_allocator.hpp
@@ -1,13 +1,40 @@
 #ifndef INCLUDED_UNBUGGY_INFO_ALLOCATOR
 #define INCLUDED_UNBUGGY_INFO_ALLOCATOR
 
+#include <memory>
+
 namespace unbuggy {
 
-template <typename T>
+/**
+ * T - the allocated type
+ * A - the wrapped allocator type
+ */
+template <typename T, typename A =std::allocator<T> >
 class info_allocator {
+
+    A m_a;  // the wrapped allocator
+
   public:
     typedef T value_type;
+
+    /** Wrap a default-constructed instance of `A`. */
+    info_allocator();
+
+    /** Wrap a copy of `a`. */
+    info_allocator(A const& a);
 };
+
+template <typename T, typename A>
+info_allocator<T, A>::info_allocator()
+  : m_a( )
+{
+}
+
+template <typename T, typename A>
+info_allocator<T, A>::info_allocator(A const& a)
+  : m_a( a )
+{
+}
 
 } // close namespace unbuggy
 
--- src/unbuggy/info_allocator_test.cpp
+++ src/unbuggy/info_allocator_test.cpp
@@ -1,10 +1,15 @@
 #include "unbuggy/info_allocator.hpp"
 
+#include <memory>
+#include <vector>
+
 struct item {
 };
 
 int main()
 {
-    unbuggy::info_allocator<item> a;
+    unbuggy::info_allocator<item, std::allocator<item> > a;
     (void)a;
+
+    std::vector<item, unbuggy::info_allocator<item> > v(a);
 }
